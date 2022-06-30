b5908e94ba1f1bfa657d8d2b3a01c3032b202a07
Dimitrie Mennings <dmennings@tutanota.com>
2022-06-30 15:03:00 +0000

    Added login functionality

--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,14 @@
 
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, LoginManager, UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
+# Database setup
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="menster",
     password="1234-Hoedje-Van",
@@ -20,6 +23,43 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+# Login Manager
+app.secret_key = 'a verry verry secret 88 verry 77 secret kee'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+
+# User Class #############################################
+
+class User(UserMixin):
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
+
+
+# Add Users ##################################################
+all_users = {
+        'admin': User('admin', generate_password_hash('secret')),
+        'bob': User('bob', generate_password_hash('less-secret')),
+        'Caroline': User('caroline', generate_password_hash('completelysecret')),
+        'menster': User('menster', generate_password_hash('Harrie'))
+    }
+
+@login_manager.user_loader
+def load_user(user_id):
+    return all_users.get(user_id)
+
+
+
+# Database Model #############################################
+
 class Comment(db.Model):
 
     __tablename__ = 'comments'
@@ -29,6 +69,7 @@ class Comment(db.Model):
 
 
 
+# Routes #####################################################
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -42,6 +83,24 @@ def index():
         return redirect(url_for('index'))
 
 
-@app.route("/login/", methods=['GET', 'POST'])
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template("login_page.html")
+    if request.method == 'GET':
+        return render_template('login_page.html', error=False)
+
+    username = request.form['username']
+
+    if username not in all_users:
+        return render_template('login_page.html', error=True)
+
+    # get the userobject from the lost
+    user = all_users[username]
+
+    if not user.check_password(request.form['password']):
+        return render_template('login_page.html', error=True)
+
+
+    return redirect(url_for('index'))
+
+
